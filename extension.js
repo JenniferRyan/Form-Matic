9a12137342f62cab12ade61d27f6e514a4e5747a
JenniferRyan <jenniferryan.ie@gmail.com>
2016-04-13 16:28:52 +0100

    added documentation

--- extension.js
+++ extension.js
@@ -1,8 +1,22 @@
-/************************************************************************************
-  Function: storeByName - store all "name-value" pairs 
-  							  
-*************************************************************************************/
+/* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * 
+ * Function:  		storeByName 
+ *  
+ * Parameters:		nameKey:String, value:String
+ *
+ * Description:
+ *   This function is called to store data all non-empty form input fields in key-value pairs
+ *   where the key is the id/name attribute value and the value stored is the value which 
+ *   has been entered into the form input field.
+ *   It checks if a value already exists for the key, if so, it appends thee two values.
+ *
+ * Usage:
+ *  - Every non-empty form input element is stored in this way.
+ *  - Called from the extractFormData function 
+ *    within a loop the length of the form, storing one record at a time.
+ *
+ * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */						  
 function storeByName(nameKey, value){
+	
 	if(appAPI.db.get(nameKey) ){						
 		var oldValue = appAPI.db.get(nameKey);
 		var newValues = [oldValue, value];				
@@ -14,21 +28,45 @@ function storeByName(nameKey, value){
 	else{
 		appAPI.db.set(nameKey, value);					
 	}
+	
 }
 
-/************************************************************************************
-  Function: storeByURL - store URL as key and all form data as one value (object)
-  							  
-*************************************************************************************/
-function storeByURL(urlKey, urlDataArray){
-	appAPI.db.async.set(urlKey, appAPI.JSON.stringify(urlDataArray));
-
+/* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+ * Function:  		storeByURL 
+ *  
+ * Parameters:		urlKey:String, urlData:Object
+ * 
+ * Description:
+ *   This function is called to store all data from a form as one record
+ *   where the key is the website url and the value is an object of key-pair values
+ *
+ * Usage:
+ *  - Data is stored in this way only when the url has not been previously stored 
+ *    or if the user chooses to overwrite previously stored data.
+ *  - Useful when populating a previously submitted form
+ *
+ * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */  
+function storeByURL(urlKey, urlData){
+	
+	appAPI.db.async.set(urlKey, appAPI.JSON.stringify(urlData));
+	
 }
 
-/************************************************************************************
-  Function: matchURLdata - take data stored for a URL and match with form fields
-  							  
-*************************************************************************************/
+/* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+ * Function:  matchURLdata 
+ *  
+ * Description:
+ *   This function is called to get the all key-pair values which have been stored
+ *   as one value (as an "object") where the key is the website url 
+ *   and the value is an object of key-pair values.
+ *   Each of the keys is compared to the id/name attribute value of the form,
+ *   when a match is found, the stored value is output into the matching textbox.
+ *
+ * Usage:
+ *  Data is output in this way when the user revisits a form that has been 
+ *  previously subitted.
+ *
+ * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */  
 function matchURLdata(urlObject){
 	
 	var input = document.getElementsByTagName("input"); 
@@ -49,10 +87,19 @@ function matchURLdata(urlObject){
 	}
 }
 
-/************************************************************************************
-  Function: extractFormData - get form data 
-  							  
-*************************************************************************************/
+/* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+ * Function:  		extractFormData 
+ *
+ * Parameters:		currentUrl:String, urlFound:Boolean
+ *  
+ * Description:
+ *   This function is called to get all data that is input into a web form, as well as 
+ *   the id/name attribute value given to each of the input elements.
+ *
+ * Usage:
+ *   Called on page load when a page contains a form 
+ *
+ * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */ 
 function extractFormData(current_url, urlFound){
 	var input = document.getElementsByTagName("input"); 
 	var names = [];
@@ -106,11 +153,16 @@ function extractFormData(current_url, urlFound){
     }
 }
 
-
-/************************************************************************************
-  Function: appAPI.ready() - anything within this scope, runs on page load
-  							 
-*************************************************************************************/
+/* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+ * appAPI.ready() 
+ *
+ * Description:
+ *   Every extension that runs on every page, must have a single appAPI.ready method
+ *  
+ * Usage:
+ *   Everything within this scope is run when the page's DOM is ready 
+ *
+ * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */ 
 appAPI.ready(function($) { 
 
     appAPI.resources.jQuery('1.8.3');
@@ -141,6 +193,7 @@ appAPI.ready(function($) {
 	var urlFound = false;	
 	var form = document.getElementsByTagName("form");
 	
+	// Only runs if a form exists
 	if(form){	
 		appAPI.db.async.get(url, function(value) {
 	        if(value === '[]' || value === undefined){	
@@ -162,12 +215,9 @@ appAPI.ready(function($) {
 	        	searchStoredData();		
 	        }
 		});
-		
-	
 	
 	// Search through form fields for data extraction, listening for a "submit" event
 		for(var i=0; i < form.length; i++){
-			
 		/*	var hasChangedCount = 0;
 			
 			form[i].addEventListener("click", function(){
@@ -175,7 +225,6 @@ appAPI.ready(function($) {
 			//	alert(hasChangedCount);
 			}, false);
 		*/
-		
 		//	if(hasChangedCount > 0){
 		//		alert("true");
 				form[i].addEventListener("submit", function(){
@@ -183,84 +232,98 @@ appAPI.ready(function($) {
 				}, false);
 		//	}
 		}
-		
 	}
 	
-/************************************************************************************
-	  Function: searchStoredData - get data stored by Name key 
-	  							  using Crossrider app
-*************************************************************************************/	
-function searchStoredData(){   
-		
-	var input = document.getElementsByTagName("input"); 
-	var storedItems = appAPI.db.getList(); 
-	var excludeSearch = null;
-
-	for(var i=0; i < input.length; i++){
-		
-	    for(var j=0; j<storedItems.length; j++) {
-	    	// pattern match against stored data for any key with "search" or "q" - to be excluded, in order to prevent autofilling form search fields
-	    	excludeSearch = (storedItems[j].key).match(/search|q/i); // returns null if no match is found 
-	       
-	        if((storedItems[j].key === input[i].getAttribute('id') || storedItems[j].key === input[i].getAttribute('name')) && excludeSearch === null){    
-	        
-	        	if(typeof(storedItems[j].value) === 'object'){ 
-		    		
-		    		var storedValues = appAPI.JSON.stringify(storedItems[j].value);
-		    		var regEx = /\s*,\s*/;						
-					var valueList = storedValues.split(regEx);  
-					
-					var arrayOfValues = appAPI.JSON.parse(valueList);
-					var textBoxName="";
-					var attributeType="";
-					
-					if(input[i].getAttribute('id')){
-						textBoxName = input[i].getAttribute('id');
-						attributeType="id";
-					}
-					else{
-						textBoxName = input[i].getAttribute('name');
-						attributeType="name";
-					}
-					appendOptions(arrayOfValues, textBoxName, attributeType); 
-		    	
-	    		}
-	    		else{ 
-	    			input[i].value = storedItems[j].value; 	
-	    		}
-	        }
-	    } 
+	/* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+	 * Function:  		searchStoredData 
+	 *  
+	 * Description:
+	 *   This function is called to search through all indiividually stored key-pair values
+	 *
+	 * Usage:
+	 *   - Called when the url for the form has not been found, i.e. it's the first visit
+	 *     to this form.
+	 *   - Pattern matching used to exclude stored values for search input fields
+	 *
+	 * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */ 
+	function searchStoredData(){   
+			
+		var input = document.getElementsByTagName("input"); 
+		var storedItems = appAPI.db.getList(); 
+		var excludeSearch = null;
+	
+		for(var i=0; i < input.length; i++){
+			
+		    for(var j=0; j<storedItems.length; j++) {
+		    	// pattern match against stored data for any key with "search" or "q" - to be excluded, in order to prevent autofilling form search fields
+		    	excludeSearch = (storedItems[j].key).match(/search|q/i); // returns null if no match is found 
+		       
+		        if((storedItems[j].key === input[i].getAttribute('id') || storedItems[j].key === input[i].getAttribute('name')) && excludeSearch === null){    
+		        
+		        	if(typeof(storedItems[j].value) === 'object'){ 
+			    		
+			    		var storedValues = appAPI.JSON.stringify(storedItems[j].value);
+			    		var regEx = /\s*,\s*/;						
+						var valueList = storedValues.split(regEx);  
+						
+						var arrayOfValues = appAPI.JSON.parse(valueList);
+						var textBoxName="";
+						var attributeType="";
+						
+						if(input[i].getAttribute('id')){
+							textBoxName = input[i].getAttribute('id');
+							attributeType="id";
+						}
+						else{
+							textBoxName = input[i].getAttribute('name');
+							attributeType="name";
+						}
+						appendOptions(arrayOfValues, textBoxName, attributeType); 
+			    	
+		    		}
+		    		else{ 
+		    			input[i].value = storedItems[j].value; 	
+		    		}
+		        }
+		    } 
+		} 
+	}
+	
+	/* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+	 * Function:  		appendOptions 
+	 *
+	 * Parameters:		arrayOfValues:Array, textBoxName:String, attributeType:String
+	 *  
+	 * Description:
+	 *   This function is called when multiple values have been stored for a particular 
+	 *   key. It allows the user to select which value to insert into the textbox.
+	 *
+	 * Usage:
+	 *   - Called from searchStoredData, only if required
+	 *   - Must be called on page load for jQuery dependancies
+	 *
+	 * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */ 
+	function appendOptions(arrayOfValues, textBoxName, attributeType){
+	
+		if(attributeType === "id"){
+			$("input[id*= " + textBoxName + "]").autocomplete({
+			  autoFocus: true,
+			  source: arrayOfValues,
+			  minLength: 0,
+			  scroll: true
+			}).focus(function() {
+			  	$(this).autocomplete("search", "");  
+			});
+		}
+		else{
+			$("input[name*= " + textBoxName + "]").autocomplete({
+			  source: arrayOfValues,
+			  minLength: 0,
+			  scroll: true
+			}).focus(function() {
+			  $(this).autocomplete("search", ""); 
+			});
+		}
 	} 
-}
-	
-/************************************************************************************
-  Function: appendOptions 
-  						
-*************************************************************************************/	
-function appendOptions(arrayOfValues, textBoxName, attributeType){
 
-	if(attributeType === "id"){
-		$("input[id*= " + textBoxName + "]").autocomplete({
-		  autoFocus: true,
-		  source: arrayOfValues,
-		  minLength: 0,
-		  scroll: true
-		}).focus(function() {
-		  	$(this).autocomplete("search", "");  
-        	//I moved the autoFocus option to be triggered here (inside the .Focus event (NOT WORKING!!!!)
-        //	$("input[id*= " + textBoxName + "]").autocomplete({ autoFocus: true });
-		});
-	}
-	else{
-		$("input[name*= " + textBoxName + "]").autocomplete({
-		  source: arrayOfValues,
-		  minLength: 0,
-		  scroll: true
-		}).focus(function() {
-		  $(this).autocomplete("search", ""); 
-		});
-	}
-} 
-		
-		    
 });
